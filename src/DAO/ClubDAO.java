6363c4479ee047f3d0fd4f371e38faf5928b3997
Rania Ayadi <rania.ayadi.ar@gmail.com>
2017-12-02 18:20:07 +0100

    Add files via upload

---
+++ src/DAO/ClubDAO.java
@@ -0,0 +1,19 @@
+package DAO;
+
+import java.sql.Date;
+import java.util.List;
+
+public interface ClubDAO {
+	//L'ajout
+	void create (Club club );
+	
+	//Consultation totale du table
+	List<Club> findAll();
+	
+	//Mise à jour d'un 
+	
+	//Selection par un champ unique
+	Club findByLabel(String label);
+	
+	List<Club> findAllAfterDateCreation (Integer year);
+}
